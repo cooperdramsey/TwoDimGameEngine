1db5c196794b3c3aa377eba21d1a109d91b86f89
Cooper Ramsey <cooperdramsey@gmail.com>
2018-08-02 12:14:09 -0600

    Create header files for string, iostream conversion between unicode and multibyte sets

---
+++ WinMain.cpp
@@ -0,0 +1,21 @@
+#include "WinMain.h"
+
+//Additional inclusions
+#ifndef _STRING_H
+#include "string.h"
+#endif
+#ifndef _IOSTREAM_H
+	#include "iostream.h"
+#endif
+
+#if defined (DEBUG) | defined(_DEBUG)
+int _tmain(int argc, char* argv[])
+{
+
+}
+#endif
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+}
